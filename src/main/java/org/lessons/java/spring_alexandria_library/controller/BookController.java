8056e1e684ff6929592f047853687d1a3c06d9db
giannivitale <giavitale78@gmail.com>
2026-05-21 12:20:58 +0200

    Add book creation form and validation; enhance index page with create button

--- src/main/java/org/lessons/java/spring_alexandria_library/controller/BookController.java
+++ src/main/java/org/lessons/java/spring_alexandria_library/controller/BookController.java
@@ -7,10 +7,15 @@ import org.lessons.java.spring_alexandria_library.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/books")
@@ -55,5 +60,19 @@ public class BookController {
         return "books/create";
     }
 
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("book") Book formBook, 
+    BindingResult bindingResult, 
+    Model model ) {
+        if (bindingResult.hasErrors()){
+            return "books/create";
+        }else{
+            // salvare il dato
+            repository.save(formBook);
+            return "redirect:/books";
+
+        }
+
+    }
 }
 
